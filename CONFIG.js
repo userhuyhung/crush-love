84618e11b8628b2d970b94255fb3cdfb6f72b75f
userhuyhung <89526306+userhuyhung@users.noreply.github.com>
2022-06-12 02:42:26 +0700

    Update CONFIG.js

--- CONFIG.js
+++ CONFIG.js
@@ -6,7 +6,7 @@ const CONFIGDATA = {
   buttonNo: "Không! Còn lâu nhé.",
   titleModar: "Cảm ơn chị vì đồng ý",
   descriptionModar:
-    'Em biết là chị sẽ đồng ý làm người yêu em mà ^^ </br> Chị là đóa hoa đẹp nhất em sẽ che chở cho chị cả cuộc đời</br> "Thích chị nhiều đến mấy, chị sẽ hiểu thôi. </br>  Em thích chị nhiều đến mấy, chị sẽ hiểu thôi "',
+    'Em biết là chị sẽ đồng ý làm người yêu em mà ^^ </br> Chị là đóa hoa mãi mãi không tàn ',
 };
 
   
